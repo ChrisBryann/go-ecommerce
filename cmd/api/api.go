7f14136180704d79ff42f7959f67a6cf9f2747ac
Christopher Bryan <christopher_bryan@rocketmail.com>
2025-12-17 15:40:02 -0800

    initial commit

---
+++ cmd/api/api.go
@@ -0,0 +1,38 @@
+package api
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+
+	"github.com/ChrisBryann/go-ecommerce/services/users"
+	"github.com/gorilla/mux"
+)
+
+type APIServer struct {
+	addr string
+	db   *sql.DB
+}
+
+func NewAPIServer(addr string, db *sql.DB) *APIServer {
+	return &APIServer{
+		addr: addr,
+		db:   db,
+	}
+}
+
+func (s *APIServer) Run() error {
+	router := mux.NewRouter()
+	subrouter := router.PathPrefix("/api/v1").Subrouter()
+
+	usersHandler := users.NewHandler()
+	usersHandler.RegisterRoutes(subrouter)
+
+	log.Println("Listening on", s.addr)
+
+	return http.ListenAndServe(s.addr, router)
+}
+
+func (s *APIServer) HelloWorld(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!"))
+}
